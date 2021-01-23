a5183e86c9c143f79141d7b70f11a1d92ab57e0a
Youssef <youssef.najeh@smals.be>
2021-01-23 17:36:11 +0100

    create link between our activities

--- app/src/main/java/ma/pharmaconnect/pharmaconnect/CreateAccountActivity.java
+++ app/src/main/java/ma/pharmaconnect/pharmaconnect/CreateAccountActivity.java
@@ -1,6 +1,9 @@
 package ma.pharmaconnect.pharmaconnect;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,15 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+    }
+
+    public void onClickButton(View view) {
+        Intent intent =new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickTextHaveAlreadyAccount(View view) {
+        Intent intent =new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/ma/pharmaconnect/pharmaconnect/LoginActivity.java
+++ app/src/main/java/ma/pharmaconnect/pharmaconnect/LoginActivity.java
@@ -1,8 +1,10 @@
 package ma.pharmaconnect.pharmaconnect;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onClickButton(View view) {
+        Intent intent =new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickTextDontHaveAccount(View view) {
+        Intent intent =new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
     }
 }
