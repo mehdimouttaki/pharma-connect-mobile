db1f814bed457c8238aac165e6e1d6b5c59ddbda
Youssef <youssef.najeh@smals.be>
2021-01-23 18:39:47 +0100

    add profile and main activity

--- app/src/main/java/ma/pharmaconnect/pharmaconnect/ProfileActivity.java
+++ app/src/main/java/ma/pharmaconnect/pharmaconnect/ProfileActivity.java
@@ -3,12 +3,16 @@ package ma.pharmaconnect.pharmaconnect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
+    }
+
+    public void onClickButtonModifyInfo(View view) {
     }
 }
