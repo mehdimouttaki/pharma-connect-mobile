5edfcddbd418522127a581aa13eaccec578b049a
mehdimouttaki <mehdimouttaki99@gmail.com>
2021-04-02 00:04:45 +0100

    Add new logo in  Application (Pharma Connect)

--- app/src/main/java/ma/pharmaconnect/pharmaconnect/Constant.java
+++ app/src/main/java/ma/pharmaconnect/pharmaconnect/Constant.java
@@ -1,5 +1,5 @@
 package ma.pharmaconnect.pharmaconnect;
 
 public abstract class Constant {
-    public static final String BASE_URL = "http://192.168.1.106:8080";
+    public static final String BASE_URL = "http://192.168.0.167:8080";
 }
